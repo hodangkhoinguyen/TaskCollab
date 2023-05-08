79ff4a1b12e626f6d1daf77da0ec8502e9a14d44
Harman <hshothi@ucdavis.edu>
2023-05-07 20:44:43 -0700

    Signup and Login pages

---
+++ frontend/src/pages/Login/login.jsx
@@ -0,0 +1,56 @@
+
+import React, { useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ // const navigate = useNavigate();
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic here
+    // You can access the entered email and password using the 'email' and 'password' variables
+    // navigate.push('/main');
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-form">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <p>Don't have an account? <a href="/signup">Sign up</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
